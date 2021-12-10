fc8028e649d38ac4317627408ce562227267eb41
Sammy Tran <sammy.tran1@ucalgary.ca>
2021-12-10 12:56:33 -0700

    added more endpoints

--- server/index.js
+++ server/index.js
@@ -23,6 +23,74 @@ app.get("/api/get", (req, res) => {
     })
 })
 
+//get password?
+
+//get preference
+
+//set user information
+
+//nutrition value
+app.get("api/recipe_nutritionValue", (req, res) => {
+
+});
+
+//ingredients
+app.get("api/recipe_ingredients", (req, res) => {
+
+});
+
+//cooking method
+app.get("api/recipe_cookingMethod", (req, res) => {
+
+});
+
+//allergy
+app.get("api/recipe_allergy", (req, res) => {
+
+});
+
+//origin country
+app.get("api/recipe_country", (req, res) => {
+
+});
+
+//recipe type
+app.get("api/recipe_type", (req, res) => {
+
+});
+
+//cooking equipment
+app.get("api/cookingEquipment", (req, res) => {
+
+});
+
+//get review
+app.get("api/get_recipe_review", (req, res) => {
+
+});
+
+//get review time 
+app.get("api/get_recipe_reviewTime", (req, res) => {
+
+});
+
+//get review by time?
+
+//set review
+app.post("api/set_recipe_review", (req, res) => {
+
+});
+
+//set ratings
+app.post("api/set_recipe_ratings", (req, res) => {
+
+});
+
+//get ratings
+app.get("api/get_recipe_ratings", (req, res) => {
+
+});
+
 app.use(express.json()); //parse incoming requests to JSON
 app.use(cors());
 
